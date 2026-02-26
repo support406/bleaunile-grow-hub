ba7c37313e451448b880c800450941d893930976
support406 <support@park-row.co.uk>
2026-02-26 17:33:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({ mode }) => ({
+export default defineConfig(({ mode }) => ({
   base: "/",
   server: {
+    host: "::",
     port: 8080,
     hmr: {
       overlay: false,
